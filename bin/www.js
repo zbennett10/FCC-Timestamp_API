590afda097e7b03e38f5bb07d6fe58e86994216d
zbennett10 <zbennett10@Hotmail.com>
2016-11-28 20:23:29 -0500

    minor updates

--- bin/www.js
+++ bin/www.js
@@ -8,3 +8,20 @@ app.set('port', port);
 var server = http.createServer(app);
 
 server.listen(port);
+
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
